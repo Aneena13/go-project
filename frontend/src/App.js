610d884335c3603b189a24bccbf9d4bd19f8dffb
theresa <theresatstephen@gmail.com>
2023-06-03 20:00:06 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,23 @@ import Myjourney from "./component/Myjourney/Myjourney";
 import Login from "./component/Login/Login";
 import Home from "./component/Home/Home";
 import Signup from "./component/Signup/signup";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Profile from "./component/Profile/Profile";
 import Admin from "./component/Admin/Admin";
 import Search from "./component/Search/Search";
 
-
 export default () => {
-  return(
+  return (
     <BrowserRouter>
-    <Routes>
-     <Route path="/" exact element={<Login />} />
-     <Route path="/signup" exact element={<Signup />} />
-     <Route path="/home" element={<Home />} />
-     <Route path="/profile" element={<Profile />} />
-     <Route path="/admin" element={<Admin />} />
-     <Route path="/search" element={<Search />} />
-    </Routes>
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route path="/signup" exact element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/myjourney" element={<Search />} />
+        <Route path="/search/:source/:destination" element={<Search />} />
+      </Routes>
     </BrowserRouter>
   );
 };
